14857fedfd3f9f4131c85e921ec947c249662ba7
Данил <mysc1@yandex.ru>
2023-08-25 18:44:45 +0300

    lesson 12

--- advertisements/urls.py
+++ advertisements/urls.py
@@ -6,7 +6,3 @@ urlpatterns = [
     path('top-sellers/', top_sellers, name='sellers'),
     path('advertisement/', advertisement_post, name='advert_post'),  # обьявления http://127.0.0.1:8000/advertisement/
 ]
-
-
-
-
--- app_auth/urls.py
+++ app_auth/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import my_login
+from .views import my_login, profile, my_logout
 
 
 urlpatterns = [
     path("login/", my_login, name="login"),
+    path("profile/", profile, name="profile"),
+    path("logout/", my_logout, name="logout"),
 ]
